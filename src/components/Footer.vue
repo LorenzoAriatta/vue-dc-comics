7047d43f25bb49c4c599a422eacc3bea2072ea7a
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-04-06 15:13:53 +0200

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer></footer>
+</template>
+
+<script>
+export default {
+  name: "FooterSection",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,12 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+  name: "MainSection",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
