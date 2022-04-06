d116e21156500ff5aa10b136e19c6a51fc2d4619
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-04-06 15:29:34 +0200

    impostato header con aggiunta classi

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,42 @@
 <template>
-  <div></div>
+  <header>
+    <img src="../assets/dc-logo.png" alt="DC-Logo" />
+    <ul>
+      <li>CHARACTERS</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
+    </ul>
+  </header>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
-  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+header {
+  background-color: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  li {
+    list-style-type: none;
+    display: inline;
+    margin: 1em;
+    font-weight: bold;
+    font-size: 14px;
+    color: black;
+  }
+}
 </style>
