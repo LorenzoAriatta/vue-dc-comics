d66d4cde5b58d7a75640eb25ca0acfa10c0c3cf5
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-04-06 16:53:51 +0200

    completato bonus | init footer part

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,107 @@
 <template>
-  <footer></footer>
+  <footer>
+    <div id="main-footer" class="container">
+      <div class="info">
+        <div class="list">
+          <h3>DC COMICS</h3>
+          <ul>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+          </ul>
+        </div>
+        <div class="list">
+          <h3>SHOP</h3>
+          <ul>
+            <li>
+              <a href="#">Shop DC</a>
+            </li>
+            <li>
+              <a href="#">Shop DC Collectibles</a>
+            </li>
+          </ul>
+        </div>
+        <div class="list">
+          <h3>DC</h3>
+          <ul>
+            <li>
+              <a href="#">Terms Of Use</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy (New)</a>
+            </li>
+            <li>
+              <a href="#">Ad Choices</a>
+            </li>
+            <li>
+              <a href="#">Advertising</a>
+            </li>
+            <li>
+              <a href="#">Jobs</a>
+            </li>
+            <li>
+              <a href="#">Subscriptions</a>
+            </li>
+            <li>
+              <a href="#">Talent Workshops</a>
+            </li>
+            <li>
+              <a href="#">CPSC Certificates</a>
+            </li>
+            <li>
+              <a href="#">Ratings</a>
+            </li>
+            <li>
+              <a href="#">Shop Help</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <div class="list">
+          <h3>SITES</h3>
+          <ul>
+            <li>
+              <a href="#">DC</a>
+            </li>
+            <li>
+              <a href="#">MAD Magazine</a>
+            </li>
+            <li>
+              <a href="#">DC Kids</a>
+            </li>
+            <li>
+              <a href="#">DC Universe</a>
+            </li>
+            <li>
+              <a href="#">DC Power Vista</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <img src="../assets/dc-logo-bg.png" alt="DC Logo" />
+    </div>
+    <div id="bot-footer" class="container"></div>
+  </footer>
 </template>
 
 <script>
@@ -9,4 +111,33 @@ export default {
 </script>
 
 <style scoped lang="scss">
+footer {
+  background-image: url(../assets/footer-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+#main-footer {
+  display: flex;
+  justify-content: space-around;
+}
+.info {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  .list {
+    margin-bottom: 15px;
+    h3 {
+      color: white;
+      margin-bottom: 15px;
+    }
+    li {
+      list-style-type: none;
+    }
+    a {
+      text-decoration: none;
+      color: #959595;
+      font-size: 12px;
+    }
+  }
+}
 </style>
---
+++ src/components/TopFooter.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="top-footer">
+    <div class="top-footer-container">
+      <div class="links">
+        <img
+          src="../assets/buy-comics-digital-comics.png"
+          alt="Digital Comics"
+        />
+        <h4><a href="#">DIGITAL COMICS</a></h4>
+      </div>
+      <div class="links">
+        <img
+          src="../assets/buy-comics-merchandise.png"
+          alt="Comics Merchandise"
+        />
+        <h4><a href="#">DC MERCHANDISE</a></h4>
+      </div>
+      <div class="links">
+        <img
+          src="../assets/buy-comics-subscriptions.png"
+          alt="Comics Merchandise"
+        />
+        <h4><a href="#">SUBSCRIPTION</a></h4>
+      </div>
+      <div class="links">
+        <img
+          src="../assets/buy-comics-shop-locator.png"
+          alt="Comics Merchandise"
+        />
+        <h4><a href="#">COMIC SHOP LOCATOR</a></h4>
+      </div>
+      <div class="links">
+        <img src="../assets/buy-dc-power-visa.svg" alt="Comics Merchandise" />
+        <h4><a href="#">COMIC SHOP LOCATOR</a></h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopFooter",
+};
+</script>
+
+<style scoped lang="scss">
+#top-footer {
+  background-color: #0282f9;
+}
+.top-footer-container {
+  width: 80%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 50px 0px;
+  margin: 0 auto;
+
+  .links {
+    display: flex;
+    align-items: center;
+    column-gap: 1rem;
+    img {
+      width: 40px;
+      height: 50px;
+    }
+    a {
+      text-decoration: none;
+      color: white;
+      font-weight: initial;
+    }
+  }
+}
+</style>
